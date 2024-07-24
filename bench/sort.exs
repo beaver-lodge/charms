d202100e0c05958f785fcf259087a6c389a1bf56
tsai <jackalcooper@gmail.com>
2024-07-24 11:25:55 +0800

    loop_invariant_code_motion

--- bench/sort.exs
+++ bench/sort.exs
@@ -3,25 +3,17 @@ Charms.JIT.init([ENIFTimSort, ENIFMergeSort])
 
 Benchee.run(
   %{
-    "Enum.sort" => fn arr -> Enum.sort(arr) end,
-    "enif_quick_sort" => fn arr -> ENIFQuickSort.sort(arr, :arg_err) end,
-    "enif_merge_sort" => fn arr -> ENIFMergeSort.sort(arr, :arg_err) end,
-    "enif_tim_sort" => fn arr -> ENIFTimSort.sort(arr, :arg_err) end
+    "Enum.sort" => &Enum.sort/1,
+    "enif_quick_sort" => &ENIFQuickSort.sort(&1, :arg_err),
+    "enif_merge_sort" => &ENIFMergeSort.sort(&1, :arg_err),
+    "enif_tim_sort" => &ENIFTimSort.sort(&1, :arg_err)
   },
-  inputs:
-    %{
-      "array size 10" => 10,
-      "array size 100" => 100,
-      "array size 1000" => 1000
-    }
-    |> then(fn m ->
-      # for some reason, it segfaults on linux with large array size
-      if :os.type() == {:unix, :darwin} do
-        Map.merge(m, %{"array size 65535" => 65535})
-      else
-        m
-      end
-    end),
+  inputs: %{
+    "array size 10" => 10,
+    "array size 100" => 100,
+    "array size 1000" => 1000,
+    "array size 10000" => 10000
+  },
   before_scenario: fn i ->
     Enum.to_list(1..i) |> Enum.shuffle()
   end
