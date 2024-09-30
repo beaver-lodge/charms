07a900a80d4d447ff24ac63f5b94cef414c1339d
tsai <jackalcooper@gmail.com>
2024-09-30 18:06:01 +0800

    Update sup_test.exs

--- test/sup_test.exs
+++ test/sup_test.exs
@@ -17,8 +17,10 @@ defmodule SupervisorTest do
         Charms.child_spec([ChildMod, ChildMod2], name: ChildModMerged)
       )
 
-    jit = Charms.JIT.engine(ChildModMerged)
-    assert Charms.JIT.invoke(jit, &ChildMod2.term_roundtrip/1, [100]) == 100
+    assert ChildModMerged
+           |> Charms.JIT.engine()
+           |> Charms.JIT.invoke(&ChildMod2.term_roundtrip/1, [100]) == 100
+
     Charms.JIT.destroy(ChildModMerged)
     refute Process.alive?(pid)
   end
