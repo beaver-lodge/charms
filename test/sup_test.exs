40be89fef369bcc9077b0464a212e7ac978476a0
tsai <jackalcooper@gmail.com>
2024-09-30 13:05:52 +0800

    Update sup_test.exs

--- test/sup_test.exs
+++ test/sup_test.exs
@@ -2,14 +2,16 @@ defmodule SupervisorTest do
   use ExUnit.Case
 
   test "init module with supervisor" do
-    {:ok, _} =
+    {:ok, pid} =
       DynamicSupervisor.start_child(Charms.TestDynamicSupervisor, Charms.child_spec(ChildMod))
 
     assert ChildMod.term_roundtrip(100) == 100
+    Charms.JIT.destroy(ChildMod)
+    refute Process.alive?(pid)
   end
 
   test "init merged modules with supervisor" do
-    {:ok, _pid} =
+    {:ok, pid} =
       DynamicSupervisor.start_child(
         Charms.TestDynamicSupervisor,
         Charms.child_spec([ChildMod, ChildMod2], name: ChildModMerged)
@@ -17,5 +19,7 @@ defmodule SupervisorTest do
 
     jit = Charms.JIT.get(ChildModMerged)
     assert Charms.JIT.invoke(jit, &ChildMod2.term_roundtrip/1, [100]) == 100
+    Charms.JIT.destroy(ChildModMerged)
+    refute Process.alive?(pid)
   end
 end
