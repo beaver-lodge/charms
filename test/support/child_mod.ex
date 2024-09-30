a88c173132037367836b49807132326ea8f6a85d
tsai <jackalcooper@gmail.com>
2024-09-30 13:00:46 +0800

    Update child_mod.ex

--- test/support/child_mod.ex
+++ test/support/child_mod.ex
@@ -6,3 +6,13 @@ defmodule ChildMod do
     func.return(i)
   end
 end
+
+defmodule ChildMod2 do
+  use Charms
+  alias Charms.Term
+
+  defm term_roundtrip(env, i) :: Term.t() do
+    i = call ChildMod.term_roundtrip(env, i) :: Term.t()
+    func.return(i)
+  end
+end
