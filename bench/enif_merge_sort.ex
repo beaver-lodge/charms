c2d31d3eaba909728403cc3671c74ed17636a3b5
tsai <jackalcooper@gmail.com>
2024-10-05 11:53:29 +0800

    Update enif_merge_sort.ex

--- bench/enif_merge_sort.ex
+++ bench/enif_merge_sort.ex
@@ -108,7 +108,7 @@ defmodule ENIFMergeSort do
       Pointer.store(list, movable_list_ptr)
       len = Pointer.load(i32(), len_ptr)
       arr = Pointer.allocate(Term.t(), len)
-      call ENIFTimSort.copy_terms(env, movable_list_ptr, arr) :: []
+      call ENIFTimSort.copy_terms(env, movable_list_ptr, arr)
       zero = const 0 :: i32()
       do_sort(arr, zero, len - 1)
       ret = enif_make_list_from_array(env, arr, len)
