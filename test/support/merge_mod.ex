1ceafd05c6f19e577287aeb8144c60b3b417a461
tsai <jackalcooper@gmail.com>
2024-10-20 15:51:29 +0800

    Update merge_mod.ex

--- test/support/merge_mod.ex
+++ test/support/merge_mod.ex
@@ -22,8 +22,7 @@ defmodule SubMod1 do
   alias Charms.Term
 
   defm get_term(env, i) :: Term.t() do
-    i = call SubMod0.get_term(env, i)
-    func.return(i)
+    SubMod0.get_term(env, i)
   end
 
   @a __MODULE__
