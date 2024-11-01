a2c7e3548f58dd6aeda32e66294a58c2d94d055b
tsai <jackalcooper@gmail.com>
2024-11-02 04:12:17 +0800

    address review

--- lib/charms/defm/pass/create_absent_func.ex
+++ lib/charms/defm/pass/create_absent_func.ex
@@ -30,46 +30,49 @@ defmodule Charms.Defm.Pass.CreateAbsentFunc do
     block = mlirOperationGetBlock(func)
     symbol_table = mlirSymbolTableCreate(mlirOperationGetParentOperation(func))
 
-    Beaver.Walker.postwalk(
-      func,
-      MapSet.new(),
-      fn ir, created ->
-        created =
-          with op = %MLIR.Operation{} <- ir,
-               "func.call" <- MLIR.Operation.name(op),
-               {name, arg_types, ret_types} <- decompose(op),
-               true <- MLIR.is_null(mlirSymbolTableLookup(symbol_table, name)),
-               name_str <- MLIR.StringRef.to_string(name),
-               false <- MapSet.member?(created, name_str) do
-            mlir ctx: ctx, block: block do
-              {arg_types, ret_types} =
-                if s = Beaver.ENIF.signature(ctx, String.to_atom(name_str)) do
-                  s
-                else
-                  {arg_types, ret_types}
-                end
+    try do
+      Beaver.Walker.postwalk(
+        func,
+        MapSet.new(),
+        fn ir, created ->
+          created =
+            with op = %MLIR.Operation{} <- ir,
+                 "func.call" <- MLIR.Operation.name(op),
+                 {name, arg_types, ret_types} <- decompose(op),
+                 true <- MLIR.is_null(mlirSymbolTableLookup(symbol_table, name)),
+                 name_str <- MLIR.StringRef.to_string(name),
+                 false <- MapSet.member?(created, name_str) do
+              mlir ctx: ctx, block: block do
+                {arg_types, ret_types} =
+                  if s = Beaver.ENIF.signature(ctx, String.to_atom(name_str)) do
+                    s
+                  else
+                    {arg_types, ret_types}
+                  end
 
-              Func.func _(
-                          sym_name: MLIR.Attribute.string(name_str),
-                          sym_visibility: MLIR.Attribute.string("private"),
-                          function_type: Type.function(arg_types, ret_types)
-                        ) do
-                region do
+                Func.func _(
+                            sym_name: MLIR.Attribute.string(name_str),
+                            sym_visibility: MLIR.Attribute.string("private"),
+                            function_type: Type.function(arg_types, ret_types)
+                          ) do
+                  region do
+                  end
                 end
               end
+
+              MapSet.put(created, name_str)
+            else
+              _ ->
+                created
             end
 
-            MapSet.put(created, name_str)
-          else
-            _ ->
-              created
-          end
+          {ir, created}
+        end
+      )
+    after
+      mlirSymbolTableDestroy(symbol_table)
+    end
 
-        {ir, created}
-      end
-    )
-
-    mlirSymbolTableDestroy(symbol_table)
     :ok
   end
 end
