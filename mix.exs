6820123beef83fb5c9f0feedaa86bc47c0a14690
Shenghang Tsai <jackalcooper@gmail.com>
2024-12-27 20:24:06 +0800

    0.1.4-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Charms.MixProject do
   def project do
     [
       app: :charms,
-      version: "0.1.3-dev",
+      version: "0.1.4-dev",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
