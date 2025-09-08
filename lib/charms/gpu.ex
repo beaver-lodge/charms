7bb67ee5b5aca3e94a2e63319c6bc960e482b508
jackalcooper <jackalcooper@gmail.com>
2025-09-08 12:09:02 +0800

    GPU.launch_func uses keyword operands

--- lib/charms/gpu.ex
+++ lib/charms/gpu.ex
@@ -72,50 +72,18 @@ defmodule Charms.GPU do
         Arith.constant(value: Attribute.integer(MLIR.Type.index(ctx: ctx), block_z), loc: loc) >>>
           MLIR.Type.index(ctx: ctx)
 
-      operands =
-        [grid_x, grid_y, grid_z, block_x, block_y, block_z] ++
-          if cluster_size do
-            {cluster_x, cluster_y, cluster_z} = cluster_size
-            [cluster_x, cluster_y, cluster_z]
-          else
-            []
-          end ++
-          if dynamic_shared_memory_size do
-            [dynamic_shared_memory_size]
-          else
-            []
-          end ++
-          kernel_args
-
-      gridSize = if is_tuple(grid_size), do: tuple_size(grid_size), else: 1
-      blockSize = if is_tuple(block_size), do: tuple_size(block_size), else: 1
-      clusterSize = 0
-      dynamicSharedMemorySize = if(dynamic_shared_memory_size, do: 1, else: 0)
-      asyncObject = 0
-
-      operand_segments =
-        [
-          length(async_deps),
-          gridSize,
-          gridSize,
-          gridSize,
-          blockSize,
-          blockSize,
-          blockSize,
-          clusterSize,
-          clusterSize,
-          clusterSize,
-          dynamicSharedMemorySize,
-          length(kernel_args),
-          asyncObject
-        ]
-
-      operand_segment_sizes = Beaver.MLIR.ODS.operand_segment_sizes(operand_segments).(ctx)
 
       GPU.launch_func(
-        async_deps ++ operands,
+        asyncDependencies: [],
+        gridSizeX: grid_x,
+        gridSizeY: grid_y,
+        gridSizeZ: grid_z,
+        blockSizeX: block_x,
+        blockSizeY: block_y,
+        blockSizeZ: block_z,
+        kernelOperands: kernel_args,
         kernel: callee,
-        operand_segment_sizes: operand_segment_sizes,
+        operand_segment_sizes: :infer,
         loc: loc
       ) >>> []
     end
