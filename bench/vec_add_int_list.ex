3ef74b85abf530ce946b4766d0b58ac465f66585
Shenghang Tsai <jackalcooper@gmail.com>
2025-09-17 20:44:49 +0800

    upgrade beaver and fix vector

--- bench/vec_add_int_list.ex
+++ bench/vec_add_int_list.ex
@@ -7,14 +7,23 @@ defmodule AddTwoIntVec do
     i_ptr = ptr! i32()
     zero = const 0 :: Pointer.element_type(i_ptr)
     set! i_ptr[0], zero
-    init = SIMD.new(SIMD.t(i32(), 8), [0, 0, 0, 0, 0, 0, 0, 0])
+    vec = SIMD.new(SIMD.t(i32(), 8), [0, 0, 0, 0, 0, 0, 0, 0])
 
-    Enum.reduce(l, init, fn x, acc ->
+    vec = SIMD.insert(vec, 0, zero + 10000)
+    vec = SIMD.insert(vec, 1, zero + 10000)
+    vec = SIMD.insert(vec, 2, zero + 10000)
+    vec = SIMD.insert(vec, 3, zero + 10000)
+    vec = SIMD.insert(vec, 4, zero + 10000)
+    vec = SIMD.insert(vec, 5, zero + 10000)
+    vec = SIMD.insert(vec, 6, zero + 10000)
+    vec = SIMD.insert(vec, 7, zero + 10000)
+
+    Enum.reduce(l, vec, fn x, acc ->
       v_ptr = ptr! i32()
       enif_get_int(env, x, v_ptr)
       i = i_ptr[0]
       set! i_ptr[0], i + 1
-      v_ptr[0] |> vector.insertelement(acc, i)
+      SIMD.insert(acc, i, v_ptr[0])
     end)
   end
 
@@ -22,18 +31,17 @@ defmodule AddTwoIntVec do
     v1 = load_list(env, a)
     v2 = load_list(env, b)
     v = arith.addi(v1, v2)
-    start = const 0 :: i32()
 
     enif_make_list8(
       env,
-      enif_make_int(env, vector.extractelement(v, start)),
-      enif_make_int(env, vector.extractelement(v, start + 1)),
-      enif_make_int(env, vector.extractelement(v, start + 2)),
-      enif_make_int(env, vector.extractelement(v, start + 3)),
-      enif_make_int(env, vector.extractelement(v, start + 4)),
-      enif_make_int(env, vector.extractelement(v, start + 5)),
-      enif_make_int(env, vector.extractelement(v, start + 6)),
-      enif_make_int(env, vector.extractelement(v, start + 7))
+      enif_make_int(env, SIMD.extract(v, 0)),
+      enif_make_int(env, SIMD.extract(v, 1)),
+      enif_make_int(env, SIMD.extract(v, 2)),
+      enif_make_int(env, SIMD.extract(v, 3)),
+      enif_make_int(env, SIMD.extract(v, 4)),
+      enif_make_int(env, SIMD.extract(v, 5)),
+      enif_make_int(env, SIMD.extract(v, 6)),
+      enif_make_int(env, SIMD.extract(v, 7))
     )
   end
 
