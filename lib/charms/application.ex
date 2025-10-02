b38ba08cae83287647f6b3fd99eb1e459a51ffc2
jackalcooper <jackalcooper@gmail.com>
2025-10-02 22:18:00 +0800

    enable_global_debug

--- lib/charms/application.ex
+++ lib/charms/application.ex
@@ -1,5 +1,6 @@
 defmodule Charms.Application do
   @moduledoc false
+  alias Beaver.MLIR
 
   def start(_args, _type) do
     children = [
@@ -8,7 +9,8 @@ defmodule Charms.Application do
     ]
 
     if Charms.Debug.global_debug_enabled?() do
-      Beaver.MLIR.CAPI.mlirEnableGlobalDebug(true)
+      MLIR.Debug.enable_global_debug(true)
+      MLIR.Debug.set_debug_type(~w[pass-manager dialect-conversion])
     end
 
     Supervisor.start_link(children, name: __MODULE__, strategy: :one_for_one)
